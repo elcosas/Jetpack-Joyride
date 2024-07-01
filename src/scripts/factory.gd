b3b073ef1adbfa33659a20f34dbfdcda6b105ab9
elcosas <121999809+elcosas@users.noreply.github.com>
2024-06-30 17:28:28 -0700

    started on factory implementation + reorganization
    created folders for scenes and scripts respectively, created a level class, started implementation of a factory class

---
+++ src/scripts/factory.gd
@@ -0,0 +1,12 @@
+class_name Factory
+extends Node
+## generates obstacles that the player can interact with
+
+@export var obstacle: PackedScene
+
+var y_bounds: Array[int]
+
+func spawn() -> void:
+	var obstacle_inst: Node2D = obstacle.instantiate() as Node2D
+	var y_coord: int = randi_range(y_bounds.min(), y_bounds.max())
+	obstacle_inst.position.y = y_coord
---
+++ src/scripts/level.gd
@@ -0,0 +1,5 @@
+class_name Level
+extends Node2D
+## Represents a level in the game (i.e. the playable part), responsible for
+## handling factory objects and references to the player and UI
+
